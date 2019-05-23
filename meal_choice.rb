0852e533db575b05adefb6776d2a442a4e89da58
Fekade Senbeta <fekadesenbeta@gmail.com>
2019-05-23 22:17:30 +0000

    Automatically backed up by Learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,7 +1,7 @@
 # Define a method meal_choice that returns the meal_choice that was
 # passed into it and defaults to meat.
 
-def meal_choice(other_meal)
+def meal_choice(x)
   meal = "meat"
   return meal
 end
